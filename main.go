eee80ac6b904bf3b29dd01a8cd28fc2d327a77cf
Jonath-z <jonathanzihindula95@gmail.com>
2024-06-19 20:30:02 +0200

    feat: add standard input support

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"bufio"
+	"encoding/binary"
 	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
+	"unicode/utf8"
 )
 
 func contains(s []string, str string) bool {
@@ -19,8 +22,17 @@ func contains(s []string, str string) bool {
 	return false
 }
 
-func getByteNumber(fileName string) int64 {
-	fileInfo, err := os.Stat(fileName)
+func getByteNumber(fileName *string, stdin *string) int64 {
+	if stdin != nil {
+		byteStdin := []byte(*stdin)
+		return int64(binary.Size(byteStdin))
+	}
+
+	if fileName == nil {
+		panic("file name is required")
+	}
+
+	fileInfo, err := os.Stat(*fileName)
 	if err != nil {
 		panic(err.Error())
 	}
@@ -28,8 +40,22 @@ func getByteNumber(fileName string) int64 {
 	return fileInfo.Size()
 }
 
-func getNumberOfLines(fileName string) int {
-	file, err := os.Open(fileName)
+func getNumberOfLines(fileName *string, stdin *string) int {
+	if stdin != nil {
+		count := 0
+		lines := strings.NewReader(*stdin)
+		scanner := bufio.NewScanner(lines)
+		for scanner.Scan() {
+			count++
+		}
+		return count
+	}
+
+	if fileName == nil {
+		panic("file name is required if there not standard input")
+	}
+
+	file, err := os.Open(*fileName)
 	if err != nil {
 		panic(err.Error())
 	}
@@ -110,34 +136,73 @@ func ProcessFile(fileName string, cb func(string)) {
 	cb(fileName)
 }
 
+func handleStandardInput() string {
+	input := ""
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		input += scanner.Text() + "\n"
+	}
+	return input
+}
+
+const (
+	byteNumberFlag       = "c"
+	linesCounterFlag     = "l"
+	wordsCounterFlag     = "w"
+	characterCounterFlag = "m"
+)
+
 func main() {
-	byteNumber := flag.String("c", "", "Flag to get the file byte number")
-	lineNumber := flag.String("l", "", "Flag to get the file line number")
-	wordCounter := flag.String("w", "", "Flag to get the file word counter")
-	characterCounter := flag.String("m", "", "Flag to get the file character counter")
+
+	args := os.Args[1:]
+
+	if len(args) == 1 {
+		flag_ := args[0][len(args[0])-1:]
+
+		switch flag_ {
+		case byteNumberFlag:
+			stdin := handleStandardInput()
+			fmt.Println(getByteNumber(nil, &stdin))
+		case linesCounterFlag:
+			stdin := handleStandardInput()
+			fmt.Println(getNumberOfLines(nil, &stdin))
+		case wordsCounterFlag:
+			stdin := handleStandardInput()
+			wordsSlice := strings.Fields(stdin)
+			fmt.Println(len(wordsSlice))
+		case characterCounterFlag:
+			stdin := handleStandardInput()
+			runes := utf8.RuneCountInString(stdin)
+			fmt.Println(runes)
+		}
+		return
+	}
+
+	byteNumber := flag.String(byteNumberFlag, "", "Flag to get the file byte number")
+	lineNumber := flag.String(linesCounterFlag, "", "Flag to get the file line number")
+	wordCounter := flag.String(wordsCounterFlag, "", "Flag to get the file word counter")
+	characterCounter := flag.String(characterCounterFlag, "", "Flag to get the file character counter")
 
 	flag.Parse()
 
 	if *byteNumber != "" {
 		ProcessFile(*byteNumber, func(s string) {
-			fmt.Println(getByteNumber(s), s)
+			fmt.Println(getByteNumber(&s, nil), s)
 		})
 		return
 	}
-
 	if *lineNumber != "" {
 		ProcessFile(*lineNumber, func(s string) {
-			fmt.Println(getNumberOfLines(s), s)
+			fmt.Println(getNumberOfLines(&s, nil), s)
 		})
 		return
 	}
-
 	if *wordCounter != "" {
 		ProcessFile(*wordCounter, func(s string) {
 			fmt.Println(countWords(s), s)
 		})
+		return
 	}
-
 	if *characterCounter != "" {
 		ProcessFile(*characterCounter, func(s string) {
 			fmt.Println(counterCharacter(s), s)
@@ -146,11 +211,10 @@ func main() {
 	}
 
 	ProcessFile(flag.Arg(0), func(s string) {
-		byteNumber := getByteNumber(s)
-		lineNumber := getNumberOfLines(s)
+		byteNumber := getByteNumber(&s, nil)
+		lineNumber := getNumberOfLines(&s, nil)
 		wordNumber := countWords(s)
 
 		fmt.Println(byteNumber, lineNumber, wordNumber, s)
 	})
-
 }
